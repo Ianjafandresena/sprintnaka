0afc29ba4ba549cd64704e17b19932cc84d2b93b
Ianja <ianjafandresena09@gmail.com>
2024-06-24 23:16:29 +0300

    sprint2_2467

--- README.md
+++ README.md
@@ -1,1 +1,58 @@
-# sprintnaka
+# Morvenger
+Sprint Spring MVC ITU S4
+
+Source : 
+        Framework :     
+                *lib :  *framework.jar
+                        *servlet-api.jar
+                
+                *src :  *mg
+                                *itu:
+                                        *prom16 :       -FrontServlet.java 
+                        *controller:
+                                        -TestControllers.java
+                                        -Test2Controllers.java
+                        *annotation :
+                                        -AnnotationControllers.java
+                -.gitattributes
+                -compress.bat
+                -Deployement.bat
+                -Morvenger.war
+                -Readme.Md
+                -Todo.txt
+        
+        Web :   
+                *conf :  --web.xml   
+                *lib :  *framework.jar
+                        *servlet-api.jar      
+
+
+Mardi 07 Mai 2024
+    Sprint 0:
+        -   GitHub:
+                Creation Depot
+                Branche Sprint 0
+                
+        -   Script:
+                .Jar
+                Deployement
+
+        -   FrontController:
+                package = mg.itu.prom16.FrontServlet(Controller)
+                Doget/DoPost = ProcessRequest {Recuperation des urls}
+                Web.xml
+
+    Sprint 1:
+        -Annotation:
+            AnnotationControllers
+
+        -FrontServlet +=
+            getListAnnotation
+
+        -Controller
+            TestControllers:Sans Annotation
+            Test2Controllers: Avec Annotation
+
+        -Web Xml
+            initParam: 
+                contextConfigLocation:controller
--- src/mg/itu/prom16/FrontController.java
+++ src/mg/itu/prom16/FrontController.java
@@ -1,30 +1,64 @@
 package mg.itu.prom16;
 
-import java.io.File;
-import java.io.IOException;
-import java.io.PrintWriter;
-import java.util.ArrayList;
-
+import java.io.*;
+import java.lang.reflect.Method;
+import java.util.*;
+import annotation.*;
+import controller.*;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import mg.itu.prom16.Annotations.Controller;
 
 public class FrontController extends HttpServlet {
-    protected ArrayList<String> controllerArrayList = new ArrayList<String>();
 
-    public void getControllerList(String packagename) throws Exception {
-        String bin_path = "WEB-INF/classes/" + packagename.replace(".", "/");
+    protected List<Class<?>> list_controller = new ArrayList<>();
+    protected Map<String, Mapping> urlMappings = new HashMap<>();
+
+    public void getControllerListSprint1(String package_name) throws ClassNotFoundException {
+        String bin_path = "WEB-INF/classes/" + package_name.replace(".", "/");
 
         bin_path = getServletContext().getRealPath(bin_path);
 
         File b = new File(bin_path);
+
+        list_controller.clear();
+        
         for (File onefile : b.listFiles()) {
             if (onefile.isFile() && onefile.getName().endsWith(".class")) {
-                Class<?> clazz = Class.forName(packagename + "." + onefile.getName().split(".class")[0]);
-                if (clazz.isAnnotationPresent(mg.itu.prom16.Annotations.Controller.class))
-                    controllerArrayList.add(clazz.getName());
+                Class<?> clazz = Class.forName(package_name + "." + onefile.getName().split(".class")[0]);
+                if (clazz.isAnnotationPresent(annotation.AnnotationController.class)){
+                    list_controller.add(clazz.getName());
+                }
+                
+            }
+        }
+    }
+
+    protected void getControllerList(String package_name) throws ClassNotFoundException {
+        String bin_path = "WEB-INF/classes/" + package_name.replace(".", "/");
+        bin_path = getServletContext().getRealPath(bin_path);
+        File b = new File(bin_path);
+
+        list_controller.clear();
+        
+        if (b.isDirectory()) {
+            for (File onefile : b.listFiles()) {
+                if (onefile.isFile() && onefile.getName().endsWith(".class")) {
+                    String className = package_name + "." + onefile.getName().replace(".class", "");
+                    Class<?> clazz = Class.forName(className);
+                    if (clazz.isAnnotationPresent(AnnotationController.class)) {
+                        list_controller.add(clazz);
+
+                        for (Method method : clazz.getMethods()) {
+                            if (method.isAnnotationPresent(GetAnnotation.class)) {
+                                Mapping mapping = new Mapping(clazz.getName(), method.getName());
+                                String key = method.getAnnotation(GetAnnotation.class).value();
+                                urlMappings.put(key, mapping);
+                            }
+                        }
+                    }
+                }
             }
         }
     }
@@ -32,32 +66,65 @@ public class FrontController extends HttpServlet {
     @Override
     public void init() throws ServletException {
         super.init();
-    }
-
-    @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        processRequest(req, resp);
-    }
-
-    @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        processRequest(req, resp);
-    }
-
-    protected void processRequest(HttpServletRequest req, HttpServletResponse resp)
-            throws ServletException, IOException {
-        PrintWriter out = resp.getWriter();
         try {
-            getControllerList(getServletContext().getInitParameter("controllerPackage"));
+            getControllerList(getServletContext().getInitParameter("contextConfigLocation"));
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+            throw new ServletException("Failed to load controllers", e);
+        }
+    }
 
-            for (String contr : controllerArrayList) {
-                out.println(contr);
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+
+        String url = request.getRequestURI().substring(request.getContextPath().length());
+        
+        try (PrintWriter out = response.getWriter()) {
+            Mapping mapping = urlMappings.get(url);
+            out.println("<H1>Sprint S4</H1>");
+            out.println("<H3>Sprint 0</H3>");
+            out.println(request.getRequestURI());
+            out.println("<H3>Sprint 1</H3>");
+
+            try {
+                getControllerListSprint1(getServletContext().getInitParameter("contextConfigLocation"));
+            } catch (ClassNotFoundException e) {
+                e.printStackTrace();
             }
-        } catch (Exception e) {
-            out.println(e.getMessage());
-            for (StackTraceElement ste : e.getStackTrace()) {
-                out.println(ste);
+    
+            try (PrintWriter out = response.getWriter()) {
+                //out.println(request.getRequestURL().toString());
+                // or out.println(request.getRequestURI());
+    
+                out.println("<ul>");
+                for (String controller : list_controller) {
+                    out.println("<li>"+controller+"</li>");
+                }
+                out.println("<ul>");
+            }
+
+
+            out.println();
+
+            if (mapping != null) {
+                out.println("URL: " + url + "</br>");
+                out.println("Associated with: " + mapping);
+            } else {
+                out.println("No Get method associated with the URL: " + url);
             }
         }
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
     }
 }
